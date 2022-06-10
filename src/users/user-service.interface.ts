19555105b97793f9ba1e1282eac208d419428825
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-10 18:35:43 +0300

    feat: add user interfaces

---
+++ src/users/user-service.interface.ts
@@ -0,0 +1,11 @@
+import { IUser } from './user.inteface';
+
+export type UserInfo = Omit<IUser, 'id'>;
+
+export interface IUserService {
+  getAllUsers: () => Promise<IUser[]>;
+  getUserById: (id: string) => Promise<IUser | null>;
+  postUser: (userInfo: UserInfo) => Promise<void>;
+  putUser: (id: string, userInfo: UserInfo) => Promise<void>;
+  deleteUser: (id: string) => Promise<void>;
+}
